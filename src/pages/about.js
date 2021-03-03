3c6a079239a38957b83ae083877e8e6dc5c5f447
Brandon <bst011988@gmail.com>
2021-03-03 13:12:36 -0500

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            This is my super awesome about page!!!
+        </div>
+    )
+}
+
+export default About
